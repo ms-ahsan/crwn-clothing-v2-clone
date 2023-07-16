7ec2913fb07217439c12f8a6b7d45684dae9fdff
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-07-16 19:30:05 +0700

    Section 10 : video 130

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 import SHOP_DATA from '../shop-data.js';
 
@@ -10,8 +10,12 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    addCollectionAndDocuments('categories', SHOP_DATA);
-  });
+    const getCategories = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategories();
+  }, []);
 
   const value = { products };
   return (
