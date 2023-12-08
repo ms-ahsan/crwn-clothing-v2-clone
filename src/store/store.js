dbf92af518768e5df13d7fe2ffb1a7d4697e7d57
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-12-08 09:54:45 +0700

    section 15 : video 170

--- src/store/store.js
+++ src/store/store.js
@@ -2,23 +2,10 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
 //middleware
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type', action.type);
-  console.log('payload', action.payload);
-  console.log('currentstate', store.getState());
-
-  next(action);
-
-  console.log('next state:', store.getState());
-};
 
 const persistConfig = {
   key: 'root',
@@ -28,9 +15,19 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleware = [loggerMiddleware];
+const middleware = [
+  process.env.NODE_ENV !== 'production' && logger,
+].filter(Boolean);
 
-const composeEnhancers = compose(applyMiddleware(...middleware));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composeEnhancers = composeEnhancer(
+  applyMiddleware(...middleware)
+);
 
 //root reducer
 
